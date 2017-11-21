ea470f22976c97ebf80e05f91eabbbf626ac4884
Isaac Rosenberg <irosenb7@gmail.com>
2017-11-21 16:48:36 -0500

    Initial Commit

---
+++ GIFSTests/GIFSTests.swift
@@ -0,0 +1,36 @@
+//
+//  GIFSTests.swift
+//  GIFSTests
+//
+//  Created by Isaac Rosenberg on 11/21/17.
+//  Copyright © 2017 Isaac Rosenberg. All rights reserved.
+//
+
+import XCTest
+@testable import GIFS
+
+class GIFSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
